489fbb5fed4dae76869e275fe23b4ead9e17def5
caique <caiquealves224@gmail.com>
2025-07-20 19:15:10 -0300

    refactor: config drizzle cli

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,6 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/libsql';
 
-export const db = drizzle(process.env.DB_FILE_NAME!);
+import * as schema from './schema';
+
+export const db = drizzle(process.env.DB_FILE_NAME!, { schema });
