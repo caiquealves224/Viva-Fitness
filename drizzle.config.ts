a8470bd7017932c97a11441519735bed96322318
caique <caiquealves224@gmail.com>
2025-07-20 18:52:01 -0300

    chore: move drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,4 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   schema: './src/db/schema.ts',
   dialect: 'sqlite',
+  dbCredentials: {
+    url: 'file:./local.db',
+  },
 });
