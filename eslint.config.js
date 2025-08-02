65020dd2bb3baf55137a90fd2d48b95aa33b1d5b
caique <caiquealves224@gmail.com>
2025-08-02 18:56:22 -0300

    feat: configure eslint

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import { URL } from 'node:url'
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: new URL('.', import.meta.url),
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+]
